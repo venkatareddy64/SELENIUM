7987459eff09e166c159a8ff40fda0894de767bb
susmitha <susmitha@192.168.100.5>
2020-03-04 12:32:39 +0530

    doneee...!

--- webdriverprograms/src/com/MQ/www/ByUsinUrlAndTitle.java
+++ webdriverprograms/src/com/MQ/www/ByUsinUrlAndTitle.java
@@ -12,11 +12,13 @@ public class ByUsinUrlAndTitle {
 		driver.get("http://www.amazon.in");
 		String title=driver.getTitle();
 		String url=driver.getCurrentUrl();
+		System.out.println(url+" "+title);
+		System.out.println("**********************************");
+
 		driver.navigate().to("http://flipkart.com");
 		String title1=driver.getTitle();
 		String url1=driver.getCurrentUrl();
-		System.out.println(title+" "+title1);
-		System.out.println(url+" "+url1);
+		System.out.println(url1+" "+title1);
 		;
 		
 	}
---
+++ webdriverprograms/src/com/MQ/www/ByUsingLinkText.java
@@ -0,0 +1,17 @@
+package com.MQ.www;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ByUsingLinkText {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://flipkart.com");
+		driver.findElement(By.linkText("Card")).click();
+		
+	}
+
+}
